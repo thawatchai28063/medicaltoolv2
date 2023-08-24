688f94024608e5442799521612962e5b00ae0be6
thawatchai  chanthakit <87002301+thawatchai28063@users.noreply.github.com>
2023-08-24 09:25:45 +0700

    ทดลองใช้ง่นGithub

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -13,41 +13,44 @@ class _SetTingPageState extends State<SetTingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        toolbarHeight: 60,
-        backgroundColor: Colors.blueAccent,
-        foregroundColor: Colors.white,
-        title: Row(
-          children: [
-            Image.asset(
-              'assets/images/Logo3.png',
-              width: 60,
-              height: 60,
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            const Text(
-              'S E T T I N G',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
-      body: Align(
-        alignment: Alignment.center, // กำหนดให้เป็นการจัดตำแหน่งกึ่งกลาง
-        child: Container(
-          width: 250, // กำหนดความกว้างของ Container
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://i0.wp.com/www.forest.go.th/khonkaen7/wp-content/uploads/sites/43/2022/02/maintence.png?ssl=1'),
-              fit: BoxFit.contain,
-            ),
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+          toolbarHeight: 60,
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/images/Logo3.png',
+                width: 60,
+                height: 60,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                'S E T T I N G',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
           ),
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width *
+                  0.8, // กำหนดความกว้างสูงสุดของ Container ให้เท่ากับความกว้างของหน้าจอ
+              maxHeight: MediaQuery.of(context)
+                  .size
+                  .height, // กำหนดความสูงสูงสุดของ Container ให้เท่ากับความสูงของหน้าจอ
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/setting.JPG"),
+                fit: BoxFit.contain, // เลือก BoxFit ที่เหมาะสมกับการแสดงรูปภาพ
+              ),
+            ),
+          ),
+        ));
   }
 }
